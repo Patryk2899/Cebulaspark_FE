297fd019e27a048652ea81390090af21687b7016
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2023-07-05 22:29:22 +0200

    Added i18next support

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,25 @@ import SignupPage from './pages/SignUpPage';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import MainPage from "./pages/MainPage";
+
+import { useTranslation } from 'react-i18next'
+import {useEffect} from "react";
+
 function App() {
     const showToastMessage = () => {
         toast.success('Success Notification !', {
             position: toast.POSITION.TOP_RIGHT
         });
     };
+
+    const { t, i18n } = useTranslation();
+
+    useEffect(() => {
+        const lng = navigator.language;
+        i18n.changeLanguage(lng);
+    }, [])
+
+    const lng = navigator.language
   return (
       <>
         <ToastContainer />
